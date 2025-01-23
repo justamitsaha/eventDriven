4710f8b29c1f461c8170c642a2f4f9b1dc694d69
justamitsaha <justamitsaha@gmail.com>
2025-01-23 18:14:34 +0530

    Create kafkaCommans.sh

---
+++ docs/kafkaCommans.sh
@@ -0,0 +1,6 @@
+docker exec --interactive --tty kafka1  kafka-topics --bootstrap-server kafka1:19092 --create --topic payment --replication-factor 3 --partitions 3
+
+
+docker exec --interactive --tty kafka1  kafka-topics --bootstrap-server kafka1:19092 --create --topic payment.RETRY --replication-factor 3 --partitions 3
+
+docker exec --interactive --tty kafka1  kafka-topics --bootstrap-server kafka1:19092 --create --topic payment.DLT --replication-factor 3 --partitions 3
