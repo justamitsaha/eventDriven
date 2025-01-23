e9d285530f1cd963cfa1a6e0b816b12db3bead7e
justamitsaha <justamitsaha@gmail.com>
2025-01-23 18:12:14 +0530

    Update PaymentService.java

--- springMessaging/src/main/java/com/saha/amit/services/PaymentService.java
+++ springMessaging/src/main/java/com/saha/amit/services/PaymentService.java
@@ -8,7 +8,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.dao.RecoverableDataAccessException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PaymentService {
 
     Log log = LogFactory.getLog(PaymentService.class);
